9ff749747203e847bf30c85b978b5f4f48372975
compwron <wordymusic@gmail.com>
2013-06-18 21:48:44 -0500

    is playable

--- lib/grid.rb
+++ lib/grid.rb
@@ -2,10 +2,11 @@ class Grid
 	attr_reader :occupied_squares
 
 	def initialize grid_size, color_count
+		@grid_size = grid_size
 		@occupied_squares = color_count * 2
 	end
 
 	def is_playable?
-		return false
+		return ! (@occupied_squares == @grid_size * @grid_size)
 	end
 end
